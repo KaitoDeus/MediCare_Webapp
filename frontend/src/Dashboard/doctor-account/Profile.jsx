4420893dbba7717fa88800954b43ca62de072d07
Khải Võ <kenkaneki395@gmail.com>
2025-11-19 19:07:39 +0700

    Tạo profile cho Bác sĩ

---
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+const Profile = () => {
+  const [formData, setformData] = useState({
+    name: "",
+    email: "",  
+    phone: "",
+    bio: "",
+  });
+
+  const handleInputChange = (e) => {};
+
+  return (
+    <div>
+      <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+        Thông tin cá nhân
+      </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Name*</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Full name"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Email"
+            className="form__input"
+            readOnly
+            aria-readonly
+            disabled="true"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Phone*</p>
+          <input
+            type="number"
+            name="phone"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Phone number"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Bio*</p>
+          <input
+            type="text"
+            name="bio"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Bio"
+            className="form__input"
+            maxLength={100}
+          />
+        </div>
+
+        <div className="mb-5">
+          <div className="grid grid-cols-3 gap-5 mb-[30px]">
+            <div>
+              <p className="form__label">Gender</p>
+              <select
+                name="gender"
+                value={formData.gender}
+                onChange={handleInputChange}
+                className="form__input py-3.5"
+              >
+                <option value="">Select</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
